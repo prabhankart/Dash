4badc67fa946afa1958824802b08404cd7d76f3a
Prabhankar Tiwari <prabhankart@gmail.com>
2024-12-25 20:20:38 +0530

    changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const connectDB = require("./db/config");
 connectDB();
-
+const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
@@ -86,6 +86,6 @@ app.get("/search/:key", async (req, res) => {
 });
 
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
